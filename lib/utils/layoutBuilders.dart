43785d9ed1b1278ec1725fcbae20abcf12f35b2d
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-07 14:47:42 -0400

    Added builder task list

--- lib/utils/layoutBuilders.dart
+++ lib/utils/layoutBuilders.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:aula_flutter_task_list/pages/home.dart';
 
+//Builder of AppBar
 Widget buildAppBar(){
   return AppBar(
     title: Text("Lista de tarefas"),
@@ -7,15 +9,21 @@ Widget buildAppBar(){
     centerTitle: true,
   );
 }
+//Builder of Body
 Widget buildBody(){
   return Column(
     children: <Widget>[
-      Container(padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
-      child: buildInputTask(),
+      Container(
+        padding: EdgeInsets.fromLTRB(10, 1, 10, 1),
+        child: buildInputTask(),
+      ),
+      Container(
+        child: buildListTask(),
       )
     ],
-  )
+  );
 }
+//Builder of InputBox and button Add of tasks
 Widget buildInputTask(){
   return Row(
           children: <Widget>[
@@ -29,10 +37,26 @@ Widget buildInputTask(){
             ),
             RaisedButton(
               color: Colors.blueAccent,
-              child: Text("Adic."),
+              child: Text("Adicionar"),
               textColor: Colors.white,
               onPressed: (){},
             )
           ],
+        );
+}
+//Builder of task list added
+Widget buildListTask(){
+  Home homePage = Home();
+  
+  return Expanded(
+          child: ListView.builder(
+            padding: EdgeInsets.only(top: 10),
+            itemCount: homePage.getList.length,
+            itemBuilder: (context, index){
+              return ListTile(
+                title: Text(homePage.getList[index]),
+              );
+            },
+          ),
         );
 }
