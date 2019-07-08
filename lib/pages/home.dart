bd470e882d68f6f1db58f0ee91d0fff8fee9f382
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-08 15:54:07 -0400

    Added fnction delete task and SnackBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,7 +9,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
   final toDoCrtl = TextEditingController();
 
   @override
@@ -112,22 +115,56 @@ class _HomeState extends State<Home> {
       child: ListView.builder(
           padding: EdgeInsets.only(top: 10),
           itemCount: _toDoList.length,
-          itemBuilder: (context, index) {
-            return CheckboxListTile(
-              title: Text(_toDoList[index]["title"]),
-              value: _toDoList[index]["completed"],
-              secondary: CircleAvatar(
-                  child: Icon(_toDoList[index]["completed"]
-                      ? Icons.check_circle
-                      : Icons.error, size: 35,)),
-              onChanged: (bool check) {
-                setState(() {
-                  _toDoList[index]["completed"] = check;
-                  _saveData();
-                });
-              },
-            );
-          }),
+          itemBuilder: buildTask),
     );
+  }
+  Widget buildTask(BuildContext context, int index){
+  return Dismissible(
+    key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+    background: Container(
+      color: Colors.red,
+      child: Align(
+        alignment: Alignment(-0.9, 0),
+        child: Icon(Icons.delete, color: Colors.white)
+        )),
+    direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+      title: Text(_toDoList[index]["title"]),
+      value: _toDoList[index]["completed"],
+      secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["completed"]
+              ? Icons.check_circle
+              : Icons.error, size: 35,)),
+      onChanged: (bool check) {
+        setState(() {
+          _toDoList[index]["completed"] = check;
+          _saveData();
+        });
+      },
+    ),
+    onDismissed: (direction){
+      setState(() {
+        _lastRemoved = Map.from(_toDoList[index]);
+        _lastRemovedPos = index;
+        _toDoList.removeAt(index);
+
+        _saveData(); 
+
+        final snack = SnackBar(
+          content: Text("tarefa " + _lastRemoved["title"] + " removida."),
+          action: SnackBarAction(
+            label: "Desfazer",
+            onPressed: (){
+              setState(() {
+                _toDoList.insert(_lastRemovedPos, _lastRemoved); 
+                _saveData();
+              });
+            }),
+          duration: Duration(seconds: 4),
+        );
+        Scaffold.of(context).showSnackBar(snack);
+      });
+    },
+  );
   }
 }
