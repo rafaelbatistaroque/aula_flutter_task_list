f39ab37c1658c066d71d2d8e60e476fff381ad37
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-07 22:11:38 -0400

    create class task with function for task list

---
+++ lib/domain/task.dart
@@ -0,0 +1,32 @@
+import 'package:path_provider/path_provider.dart';
+import 'dart:convert';
+import 'dart:io';
+
+class Task {
+  final List _toDoList = [];
+    
+  List get getList{return _toDoList;}
+  void setList(Map task) => _toDoList.add(task);
+
+  void checkList(int index, bool check){
+    _toDoList[index]["completed"] = check;
+  }
+
+  Future<File> _getFile() async{
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+  Future<File> _saveData() async{
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+  Future<String> _readData() async{
+    try{
+      final file = await _getFile();
+      return file.readAsString();
+    }catch (e){
+      return null; 
+    }
+  }
+}
